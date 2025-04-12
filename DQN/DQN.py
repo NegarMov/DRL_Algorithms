1ce8b77c43103a62345a4a366fb98d3009462287
NegarMov <movaghatian.negar@gmail.com>
2025-04-12 23:27:02 +0330

    Improve DQN to work with envs with continuous observation space

---
+++ DQN/DQN.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+from torch import optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+class DQN(nn.Module):
+    def __init__(self, ob_dim, ac_dim, hidden_layers=2, hidden_size=64, lr=1e-2):
+        super().__init__()
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        layers = []
+        in_size = ob_dim
+        for _ in range(hidden_layers):
+            layers.append(nn.Linear(in_size, hidden_size))
+            layers.append(nn.ReLU())   
+            in_size = hidden_size
+        layers.append(nn.Linear(in_size, ac_dim))
+        self.mlp = nn.Sequential(*layers).to(self.device)
+
+        parameters = self.mlp.parameters()
+
+        self.optimizer = optim.Adam(
+            parameters,
+            lr
+        )
+
+        self.loss_fn = nn.MSELoss()
+
+
+    def forward(self, ob):
+        if not isinstance(ob, torch.Tensor):
+            ob = torch.from_numpy(ob).float().to(self.device)
+
+        return self.mlp(ob)
+
+
+    def save(self, path):
+        torch.save({'model_state_dict': self.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict()}, path)
+
+
+    def load(self, path):
+        checkpoint = torch.load(path)
+        self.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
---
+++ DQN/run.bat
@@ -0,0 +1,14 @@
+@echo off
+python run.py ^
+    --env_name "CartPole-v1" ^
+    --episodes 1 ^
+    --df 0.9 ^
+    --e 1.0 ^
+    --e_min 0.1 ^
+    --e_decay_rate 0.99 ^
+    --hidden_layers 2 ^
+    --hidden_size 32 ^
+    --lr 5e-3 ^
+    --batch_size 128 ^
+    --ddqn ^
+    --eval
